aaed0442f662ae62f52d6f2d79d92ff426b3fcb6
Beyzanur-1419 <beyzanurozanalp@gmail.com>
2026-02-17 11:51:53 +0300

    App.tsx: kişisel bilgiler ve kısa tanıtım eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
+    <div style={{ textAlign: "center" }}>
       <h1>Web Tasarımı ve Programlama</h1>
       <h2>LAB-1</h2>
-      <p>Ad Soyad: Beyza Nur Ozanalp</p>
-      <p>Öğrenci No: 230541010</p>
+
+      <p><strong>Ad Soyad:</strong> Beyza Nur Ozanalp</p>
+      <p><strong>Öğrenci No:</strong> 230541010</p>
+      <p><strong>Bölüm:</strong> Bilgisayar Mühendisliği</p>
+      <p><strong>Hobiler:</strong> Kod yazmak, müzik dinlemek, yeni teknolojileri öğrenmek</p>
+
+      <p style={{ marginTop: "15px", fontStyle: "italic", color: "#555" }}>
+        Web teknolojileriyle ve mobil teknolojileriyle ilgileniyor ve kendimi bu alanda geliştirmeyi hedefliyorum.
+      </p>
     </div>
   )
 }
