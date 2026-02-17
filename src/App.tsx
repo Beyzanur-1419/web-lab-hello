f8573d9ef3b9ee82c770c62c506468ea3001e810
Beyzanur-1419 <beyzanurozanalp@gmail.com>
2026-02-17 11:43:32 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Beyza Nur Ozanalp</p>
+      <p>Öğrenci No: 230541010</p>
+    </div>
+  )
+}
+
+export default App
